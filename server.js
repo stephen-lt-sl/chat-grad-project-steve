9d5d20ff9fb1f4a7b582450f6727f72abf4a2718
Stephen Tozer <stozer@scottlogic.co.uk>
2016-08-24 16:00:17 +0100

    Fix line length

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var oAuthGithub = require("./server/oauth-github");
 var MongoClient = require("mongodb").MongoClient;
 
 var port = process.env.PORT || 8080;
-var dbUri = process.env.DB_URI || "mongodb://sample-user:Vq(Z9g^B-6hlt3Q<@ds029655.mlab.com:29655/chat-grad-project-steve";
+var dbUri = process.env.DB_URI ||
+    "mongodb://sample-user:Vq(Z9g^B-6hlt3Q<@ds029655.mlab.com:29655/chat-grad-project-steve";
 var oauthClientId = process.env.OAUTH_CLIENT_ID || "9bc77c8e1b4aa6c7221e";
 var oauthSecret = process.env.OAUTH_SECRET || "0f83b19254fc73b7c2e9f74adc7a1046bf481260";
 var devMode = process.env.DEV_MODE || false;
