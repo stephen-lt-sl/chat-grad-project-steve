95b786b23b4a642b5e1fa8366e0a55e17e074b3b
Stephen Tozer <STozer@scottlogic.com>
2016-09-09 12:07:27 +0100

    Remove reference to database from notifications module

--- server/notifications.js
+++ server/notifications.js
@@ -3,8 +3,6 @@
 module.exports = function(app, db, baseUrl) {
     var dbActions = require("./dbActions")(db);
 
-    var notifications = db.collection("notifications");
-
     app.get(baseUrl + "/notifications", function(req, res) {
         var userID = req.session.user;
         dbActions.getUserNotifications(userID).then(function(docs) {
